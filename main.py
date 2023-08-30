e615d9929fb2112f857b7767ea368a808ba25383
GachaYTB <pookiemon2019@gmail.com>
2023-08-30 15:01:13 +0200

    guesser game

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ bot.remove_command('help')
 privat = open('C:\\Users\\pooki\\OneDrive\\Bureau\\Codes\\GBot 4\\private.json', "r")
 jsondata = privat.read()
 private = json.loads(jsondata)
+typeinguessthingidk = False
 nobotsinlogs = False
 typeinchannelidthing = False
 channelidthing = 0
@@ -47,6 +48,7 @@ async def newlogsline(messagethingidk, messageauthor):
 async def on_message(msg):
     global typeinchannelidthing
     global channelidthing
+    global typeinguessthingidk
     if msg.content == 'g4:ping':
         await msg.channel.send('Pong!')
     elif msg.content == 'g4:dice':
@@ -62,8 +64,11 @@ async def on_message(msg):
         number4 = random.randint(1, 5)
         number5 = random.randint(1, 5)
         await msg.channel.send(f'Lets play now!\n1: ||{colors[number1]}||\n2: ||{colors[number2]}||\n3:||{colors[number3]}||\n4: ||{colors[number4]}||\n5: ||{colors[number5]}||')
+    elif msg.content == 'g4:guesser':
+        await msg.channel.send('Guess a number between 1 and 10 and then type it in the chat.')
+        typeinguessthingidk = True
     elif msg.content == 'g4:help':
-        await msg.channel.send('***---Logs commands---***\ng4:bilt: Activates/Deactivates bots in logs.\ng4:setlogschnlid: Sets logs channel id\n***---Other commands---***\ng4:ping: What can happen... :thinking:\ng4:help: Shows this message\ng4:ownercmds: owner commands, ask gachaytb3ondc for access.\n***---Games---***\ng4:dice: roll a dice!\ng4:hazard: Play a game of hazard!')
+        await msg.channel.send('***---Logs commands---***\ng4:bilt: Activates/Deactivates bots in logs.\ng4:setlogschnlid: Sets logs channel id\n***---Other commands---***\ng4:ping: What can happen... :thinking:\ng4:help: Shows this message\ng4:ownercmds: owner commands, ask gachaytb3ondc for access.\n***---Games---***\ng4:dice: roll a dice!\ng4:hazard: Play a game of hazard!\ng4:guesser: Guess a number between 1 and 10 and try to get the same number that the bot guessed!')
     elif msg.content == 'g4:ownercmds':
         if msg.author.id == 932666698438418522:
             await msg.author.send('***---Owner commands---***\nComing soon!')
@@ -80,6 +85,13 @@ async def on_message(msg):
     elif msg.content == 'g4:setlogschnlid':
         await msg.channel.send('Please type your channel id.')
         typeinchannelidthing = True
+    elif typeinguessthingidk == True:
+        typeinguessthingidk = False
+        number = random.randint(1, 10)
+        if int(msg.content) == number:
+            await msg.channel.send('Yay, you did it!')
+        elif msg.content != number:
+            await msg.channel.send(f'Oops, wrong answer! The correct answer was {number}.')
     elif typeinchannelidthing == True:
         typeinchannelidthing = False
         channelidthing = int(msg.content)
