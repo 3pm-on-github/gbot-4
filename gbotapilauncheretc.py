09375d7a4bce4568b38f280dc54d1d1a991983ee
user <user@whitegreenatx>
2023-09-06 21:24:08 +0100

    rearranged code, test commit

--- gbotapilauncheretc.py
+++ gbotapilauncheretc.py
@@ -1,6 +1,8 @@
 # Welcome to the gbot api launcher! You don't have to setup the gbot api by launching this script, the main.py script will do it for you.
 
 from gbotapi import gbotapi
+from numpy.core.defchararray import isnumeric
+from _xxsubinterpreters import ChannelID
 bot = None
 loop = False
 root = None
@@ -25,31 +27,30 @@ def askcommand():
     command = input('Ready')
     if "sendmsg" in command:
         error = False
-        guildid = input('Please input the guild id.')
+        guildid = input('Please input the guild id.') # is this bot designed to be a giant backdoor?
         channelid = input('Please input the channel id.')
         messagething = input('Please input the message you want to send.')
         if messagething == '':
-            print('ERROR Code 1: Message empty.')
+            print('sendmsg: ERROR 1: Message empty.')
             error = True
         try:
-            qsfdjvkjsqhfdqjnldfscqhkfqnjskfcdlqfncqhncfksqdhnfdljkqscjkklqcfhhcqchlnfdqhlkdfcqfc = str(messagething)
+            test_s = str(messagething)
         except:
-            print("ERROR Code 2: Message isn't a string.")
+            print("sendmsg: ERROR 2: Message isn't a string.")
             error = True
-        try:
-            azeczaynynezaoipurzpanuozaiocrnzepuezauipocrpoiunrczaepuiocruoirzuapcneriuinrcazpire = int(channelid)
-        except:
-            print("ERROR Code 3: Channel ID isn't an int.")
+        
+        if (not isnumeric(channelid)):
+            print("sendmsg: ERROR 3: input: \n\""+channelid+"\"\n could not be recognised as a Channel ID.")
             error = True
             if "," in channelid:
                 print("Perhaps, why did you put a comma?")
-        try:
-            cosdufudyqiybyiyuqcsuydboifqbicdbqsdyficqcfyicuisdoqbsscfqdsiuyocsfybsqcfbdsuuidcqfs = int(guildid)
-        except:
-            print("ERROR Code 4: Guild ID isn't an int.")
+        
+        if (not isnumeric(guildid)):
+            print("sendmsg: ERROR 4: input: \n\""+guildid+"\"\n could not be recognised as a Server ID.")
             error = True
             if "," in guildid:
                 print("Perhaps, why did you put a comma?")
+        
         if error == False:
             root.sendMessage(channel=channelid, guild=guildid, message=messagething)
         if loop == True:
